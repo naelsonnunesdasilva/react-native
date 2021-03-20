c812738861b97c3198e5f12d33b339092f1696a3
naelson <naelson.silva@novaescola.org.br>
2021-03-20 09:09:38 -0300

    iniciando projeto

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function App1(){
+  return (
+    <View>
+      <Text>Testando</Text>
+      <Text>Testando 2</Text>
+    </View>
+  );
+};
