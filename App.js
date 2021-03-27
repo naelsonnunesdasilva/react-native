010f417e53de29862b8a2b754182881604169964
naelson <naelson.silva@novaescola.org.br>
2021-03-27 09:49:16 -0300

    Navegação padrão

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App1(){
+const Stack = createStackNavigator();
+
+function TelaHome({ navigation }){
   return (
-    <View>
-      <Text>Testando</Text>
-      <Text>Testando 2</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        onPress={() => navigation.navigate('Canal')}
+        title="Tela Canal"/>
     </View>
+  );
+}
+
+function TelaCanal({ navigation }){
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={() => navigation.navigate('Home')} title="Tela Home" />
+    </View>
+  );
+}
+
+export default function App1() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={TelaHome} />
+        <Stack.Screen name="Canal" component={TelaCanal} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
